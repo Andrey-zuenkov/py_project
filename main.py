4561b7ec7aa48bd5d35e464618abb0756b4a0923
Andrey Zuenkov <azuenkov@yandex.ru>
2024-03-18 16:03:01 +0900

    импортировать в main.py func_subtract

--- main.py
+++ main.py
@@ -3,3 +3,5 @@ import func
 x = int(input("Введите первое число "))
 y = int(input("Введите второе число "))
 print(f'Сумма равна {func.func_sum(x,y)}')
+
+print(f'Разность равна {func.func_subtract(x,y)}')
